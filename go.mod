8e43b9208f899e19a06c2c17b3a28a434ff8584f
Joshua Biong <joshuabiong7@gmail.com>
2024-02-21 17:49:20 +0800

    adding some route of the webApp and movies and type some initial function of it

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module gomoviescrud
 
 go 1.22.0
 
-require github.com/gorilla/mux v1.8.1 // indirect
+require github.com/gorilla/mux v1.8.1
--- main.go
+++ main.go
@@ -1,1 +1,69 @@
 package main
+
+import (
+	// "encoding/json"
+	"fmt"
+	"log"
+
+	// "math/rand"
+	"net/http"
+	// "strconv"
+	"github.com/gorilla/mux"
+)
+
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
+}
+
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
+}
+
+var movies []Movie
+
+// GET ALL THE MOVIES FUNCTION
+func getMovies(w http.ResponseWriter, r *http.Request) {
+}
+
+// GET A MOVIE FUNCTION
+func getMovie(w http.ResponseWriter, r *http.Request) {
+	// code here
+}
+
+// CREATE A MOVIE
+func createMovie(w http.ResponseWriter, r *http.Request) {
+	// code here
+}
+
+// DELETE A MOVIE
+func deleteMovie(w http.ResponseWriter, r *http.Request) {
+	// code here
+}
+
+// UPDATE A MOVIE
+func updateMovie(w http.ResponseWriter, r *http.Request) {
+	// code here
+}
+
+func main() {
+
+	r := mux.NewRouter()
+
+	movies = append(movies, Movie{ID: "1", Isbn: "1234567", Title: "Movie One Title", Director: &Director{Firstname: "Joshua", Lastname: "Biong"}})
+	movies = append(movies, Movie{ID: "2", Isbn: "7654321", Title: "Movie Two Title", Director: &Director{Firstname: "Lywy", Lastname: "Biong"}})
+
+	//========ROUTES=================
+	r.HandleFunc("/movies", getMovies).Methods("GET")
+	r.HandleFunc("/movies{id}", getMovie).Methods("Get")
+	r.HandleFunc("/movies", createMovie).Methods("POST")
+	r.HandleFunc("movies{id}", updateMovie).Methods("PUT")
+	r.HandleFunc("movies{id}", deleteMovie).Methods("DELETE")
+
+	fmt.Printf("Server start in port 8000\n")
+	log.Fatal(http.ListenAndServe("locahost:8000", r))
+
+}
